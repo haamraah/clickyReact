34d31aa8e6917a80a833ddcafd8e00ff0436eaf5
KuTu <haamraah@yahoo.com>
2019-12-15 21:31:07 -0700

    Updates

---
+++ precache-manifest.c966b0e65ddc12e6310e22b548835af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d697404cab4574ec51e415a179d15ee5",
+    "url": "/clickyReact/index.html"
+  },
+  {
+    "revision": "389e71b453f5c0cb353b",
+    "url": "/clickyReact/static/css/main.a8e4d8aa.chunk.css"
+  },
+  {
+    "revision": "0800ed0fa1e4d9ca6495",
+    "url": "/clickyReact/static/js/2.46ba947a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickyReact/static/js/2.46ba947a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "389e71b453f5c0cb353b",
+    "url": "/clickyReact/static/js/main.13ffce67.chunk.js"
+  },
+  {
+    "revision": "7d9d865b53decce742df",
+    "url": "/clickyReact/static/js/runtime-main.d142ea4e.js"
+  }
+]);
